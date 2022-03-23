a142a7d8c935f87fe0b6f6d3c58ae1790ea8cea1
Jashiee <100886088+Jashiee@users.noreply.github.com>
2022-03-23 18:09:29 +1300

    Update Program.cs

--- AS91883/Program.cs
+++ AS91883/Program.cs
@@ -1,10 +1,10 @@
 ﻿/* Introduction*/
-string selection;
+int selection;
 Console.WriteLine("Hello There! This is a Te Reo Maori Quiz. The quiz will be multichoice!");
 Console.WriteLine("There are 3 different modes.");
 // QUIZ SELECTOR//
-Console.WriteLine("Please type which mode you would like to do by either typing 'beginner, Medium or hard'");
-selection = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Please type which mode you would like to do by either typing \n.1 for BEGINNER \n2. for INTERMEDIATE \n3. for ADVANCED");
+selection = Convert.ToInt32(Console.ReadLine());
 string selectionResult = selection.ToLower();
 if (selectionResult != "beginner"|| selectionResult != "medium" || selectionResult != "hard")
 {
@@ -22,3 +22,4 @@ if (selectionResult != "beginner"|| selectionResult != "medium" || selectionResu
     {
         Console.WriteLine("YOU CHOSE HARD");
     }
+ while (!int.TryParse(Console.ReadLine(), ))
