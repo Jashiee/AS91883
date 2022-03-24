fac1b762374824295372880875879b8da4387d72
Jashandeep Singh <ac116719@avcol.school.nz>
2022-03-24 15:00:10 +1300

    Added 3 questions

--- AS91883/Program.cs
+++ AS91883/Program.cs
@@ -5,29 +5,50 @@ string answer;
 Console.WriteLine("Hello There! This is a Te Reo Maori Quiz. The quiz will be multichoice!");
 Console.WriteLine("There are 3 different modes.");
 // QUIZ SELECTOR//
-Console.WriteLine("Please type which mode you would like to do by either typing \n.1 for BEGINNER \n2. for INTERMEDIATE \n3. for ADVANCED");
+Console.WriteLine("Please type which mode you would like to do by either typing \n1. for BEGINNER \n2. for INTERMEDIATE \n3. for ADVANCED");
 selection = Convert.ToInt32(Console.ReadLine());
- while (!int.TryParse(Console.ReadLine(), selection != "1" || selection != "2" || selection != "3") 
+while (!int.TryParse(Console.ReadLine(), out selection) || selection <= 0 || selection >=4)
 {
  Console.WriteLine("PLEASE ENTER A VAILD NUMBER FROM THE LIST PROVIDED");
 }
 
-if (selection == "1") 
+if (selection == 1) 
 {
  Console.WriteLine("YOU CHOSE BEGINNER");
  Console.WriteLine("QUESTION 1:");
  Console.WriteLine("What does the word Kia Ora translate to in English?");
- Console.WriteLine(A.Hello \nB.Hola \nC.Good Job \nD.Yes");
- answer = Console.ReadLine());
- if (answer == "A" || answer == "a")
-                   {
-                    Console.WriteLine
+ Console.WriteLine("A.Hello \nB.Hola \nC.Good Job \nD.Yes");
+ answer = Console.ReadLine();
+if (answer == "A" || answer == "a")
+    {
+        Console.WriteLine("Correct!");
+    }
+if (answer != "A" && answer != "a")
+    {
+        Console.WriteLine("Incorrect");
+    }
+ Console.WriteLine("QUESTION 2:");
+    Console.WriteLine("What does the word 'LOVE' mean in Maori");
+    Console.WriteLine("\nA.Love \nB.Aroha \nC.Ka Pai \nD.Whanau");
+    answer = Console.ReadLine();
+    if (answer == "B" || answer == "C")
+    {
+        Console.WriteLine("Correct");
+    }
+    if (answer != "B" && answer != "b")
+    {
+        Console.WriteLine("Incorrect");
+    }
+    Console.WriteLine("QUESTION 3:");
+    Console.WriteLine("What does the word 'Family' translate to in Maori?");
+    Console.WriteLine("A.Family \nB. \nC.Good Job \nD.Yes");
+    answer = Console.ReadLine();
 }
-If (selection == "2") 
+if (selection == 2) 
 {
  Console.WriteLine("YOU CHOSE INTERMEDIATE");
 }
-if (selection == "3")
+if (selection == 3)
 {
-Console.WriteLine("YOU CHOSE ADVANCED")'
+    Console.WriteLine("YOU CHOSE ADVANCED");
 }
