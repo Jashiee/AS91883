7516c960a372458dce95390af82e1de08a704b8b
Jashandeep Singh <ac116719@avcol.school.nz>
2022-03-25 14:11:09 +1300

    Added sound when getting answer wrong Added color

--- AS91883/Program.cs
+++ AS91883/Program.cs
@@ -2,16 +2,18 @@
 int selection;
 string a,b,c,d;
 string answer;
+string [] 
+Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("Hello There! This is a Te Reo Maori Quiz. The quiz will be multichoice!");
 Console.WriteLine("There are 3 different modes.");
 // QUIZ SELECTOR//
 Console.WriteLine("Please type which mode you would like to do by either typing \n1. for BEGINNER \n2. for INTERMEDIATE \n3. for ADVANCED");
-selection = Convert.ToInt32(Console.ReadLine());
 while (!int.TryParse(Console.ReadLine(), out selection) || selection <= 0 || selection >=4)
 {
  Console.WriteLine("PLEASE ENTER A VAILD NUMBER FROM THE LIST PROVIDED");
 }
-
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Yellow;
 if (selection == 1) 
 {
  Console.WriteLine("YOU CHOSE BEGINNER");
@@ -19,18 +21,22 @@ if (selection == 1)
  Console.WriteLine("What does the word Kia Ora translate to in English?");
  Console.WriteLine("A.Hello \nB.Hola \nC.Good Job \nD.Yes");
  answer = Console.ReadLine();
-if (answer == "A" || answer == "a")
+    Console.Clear();
+    if (answer == "A" || answer == "a")
     {
         Console.WriteLine("Correct!");
     }
 else
     {
         Console.WriteLine("Incorrect");
+        Console.Beep();
+
     }
  Console.WriteLine("QUESTION 2:");
     Console.WriteLine("What does the word 'LOVE' mean in Maori");
     Console.WriteLine("\nA.Love \nB.Aroha \nC.Ka Pai \nD.Whanau");
     answer = Console.ReadLine();
+    Console.Clear();
     if (answer == "B" || answer == "b")
     {
         Console.WriteLine("Correct");
@@ -38,37 +44,69 @@ else
   else
     {
         Console.WriteLine("Incorrect");
+        Console.Beep();
     }
     Console.WriteLine("QUESTION 3:");
     Console.WriteLine("What does the word 'Family' translate to in Maori?");
     Console.WriteLine("A.Family \nB.Ka Pai \nC.Whanau \nD.No");
     answer = Console.ReadLine();
+    Console.Clear();
     if (answer == "c" || answer == "C")
     {
         Console.WriteLine("Correct");
     }
     else 
     {
-        Console.WriteLine("Incorrect"); 
+        Console.WriteLine("Incorrect" );
+        Console.Beep();
     }
     Console.WriteLine("QUESTION 4:");
     Console.WriteLine("What does the word 'hate' translate to in Maori?");
     Console.WriteLine("A.Grape \nB.Hate \nC.Yes \nD.Mauahara");
     answer = Console.ReadLine();
-    if (answer == "D" || answer == "d")
+    Console.Clear();
+    switch (answer)
     {
-        Console.WriteLine("Correct");
+        case "a":
+            Console.WriteLine("Incorrect");
+            Console.Beep();
+            break;
+        case "b":
+            Console.WriteLine("Incorrect");
+            break;
+        case "c":
+            Console.WriteLine("Incorrect");
+            break;
+        case "d": 
+            Console.WriteLine("Correct!");
+            break;
+        case "D":
+            Console.WriteLine("Correct!");
+            break;
     }
-    else
+    Console.WriteLine("QUESTION 5:");
+    Console.WriteLine("What does the word 'Me' translate to in Maori?");
+    Console.WriteLine("A.Ka Pai \nB.Ahau \nC.Me \nD.You");
+    answer = Console.ReadLine();
+    switch (answer)
     {
-        Console.WriteLine("Incorrect");
+        case "b":
+            Console.WriteLine("Correct!");
+            break;
+        case "B":
+            Console.WriteLine("Correct!");
+            break;
     }
-    if (selection == 2)
+
+}
+Console.ForegroundColor = ConsoleColor.Green;
+if (selection == 2)
     {
         Console.WriteLine("YOU CHOSE INTERMEDIATE");
         
     }
-    if (selection == 3)
+Console.ForegroundColor = ConsoleColor.DarkRed;
+if (selection == 3)
     {
     Console.WriteLine("YOU CHOSE ADVANCED");
 }
